00847ae15d8537b2c91b541c99723b8affa9dc5f
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-12-21 22:41:16 -0800

    verify gh webhook with secret

--- webserver/public_html/github.php
+++ webserver/public_html/github.php
@@ -1,7 +1,15 @@
 <?php
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $secret = file_get_contents("/var/www/backend/gh_secret.txt");
 		$handle = fopen("/var/www/backend/github.log","w");
-		fwrite($handle, $_POST["payload"]);
+        $digest = 'sha256=' . hash_hmac('sha256',file_get_contents("php://input"), $secret);
+        if(hash_equals($_SERVER["HTTP_X_HUB_SIGNATURE_256"],$digest)) {
+            fwrite($handle, $_POST["payload"]);
+        } else {
+            fwrite($handle,"digest: $digest\n");
+            fwrite($handle,"server digest: " . $_SERVER["HTTP_X_HUB_SIGNATURE_256"] . "\n");
+            fwrite($handle,"hash mismatch");
+        }
 		fclose($handle);
 	} else {
 		header('Location: https://github.com/jedbrooke/golem-auto-editor');

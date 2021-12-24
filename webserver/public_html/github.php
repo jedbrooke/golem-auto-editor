7803822f9e0321bbe818fb4259207697dde9b6d5
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-12-23 18:42:57 -0800

    implement webhook auto-update

--- webserver/public_html/github.php
+++ webserver/public_html/github.php
@@ -1,16 +1,16 @@
 <?php
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
         $secret = file_get_contents("/var/www/backend/gh_secret.txt");
-		$handle = fopen("/var/www/backend/github.log","w");
         $digest = 'sha256=' . hash_hmac('sha256',file_get_contents("php://input"), $secret);
         if(hash_equals($_SERVER["HTTP_X_HUB_SIGNATURE_256"],$digest)) {
-            fwrite($handle, $_POST["payload"]);
+            exec("git clone -b webserver https://github.com/jedbrooke/golem-auto-editor.git /var/www/git-repo");
+            exec("rsync -ra /var/www/git-repo/webserver/public_html/ /var/www/html");
+            exec("rsync -ra /var/www/git-repo/webserver/backend/ /var/www/backend");
         } else {
             fwrite($handle,"digest: $digest\n");
             fwrite($handle,"server digest: " . $_SERVER["HTTP_X_HUB_SIGNATURE_256"] . "\n");
             fwrite($handle,"hash mismatch");
         }
-		fclose($handle);
 	} else {
 		header('Location: https://github.com/jedbrooke/golem-auto-editor');
 	}
--- webserver/public_html/hello.py
+++
@@ -1,2 +0,0 @@
-import sqlite3
-print("hello from python")

5445f0427cb84b929fc23d137b25242a1893b8a3
Jasper Edbrooke <jasperedbrooke@gmail.com>
2021-12-23 18:55:35 -0800

    small fix to github webhook action

--- webserver/public_html/github.php
+++ webserver/public_html/github.php
@@ -3,7 +3,7 @@
         $secret = file_get_contents("/var/www/backend/gh_secret.txt");
         $digest = 'sha256=' . hash_hmac('sha256',file_get_contents("php://input"), $secret);
         if(hash_equals($_SERVER["HTTP_X_HUB_SIGNATURE_256"],$digest)) {
-            exec("git clone -b webserver https://github.com/jedbrooke/golem-auto-editor.git /var/www/git-repo");
+            exec("cd /var/www/git-repo && git pull");
             exec("rsync -ra /var/www/git-repo/webserver/public_html/ /var/www/html");
             exec("rsync -ra /var/www/git-repo/webserver/backend/ /var/www/backend");
         } else {
